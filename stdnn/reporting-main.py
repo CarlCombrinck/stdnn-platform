96bf45ee2cb8615069f32a8c099c3c33f8b6dfad
wellokay <satsukihatake@gmail.com>
2022-01-15 14:34:47 +0200

    Reporting main semi-done

---
+++ stdnn/reporting-main.py
@@ -0,0 +1,30 @@
+from string import ascii_letters
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib as mt
+import os
+import sys
+
+from experiment.results import Results
+from reporting.custom_gwn_plotter import CustomGNNPlotter
+
+if __name__ == '__main__':
+    df = pd.read_csv("stdnn\data\JSE_clean_truncated.csv")
+    df_s = pd.read_csv("stdnn\data\GWN_corr.csv")
+
+    # Filepath stuff
+    absolutepath = os.path.abspath(__file__)
+    fileDirectory = os.path.dirname(absolutepath)
+    # print(fileDirectory)
+    # Navigate to Images directory
+    newpath = os.path.join(fileDirectory, 'reporting\images')
+
+    df = pd.read_csv("stdnn\data\JSE_clean_truncated.csv")
+    df_ = pd.read_csv("stdnn\data\GWN_corr.csv")
+
+    CustomGNNPlotter.plot_correlation_matrix(
+        dataframe=df, filepath=newpath+"\\")
+    CustomGNNPlotter.plot_adaptive_adj_matrix(
+        dataframe=df_, filepath=newpath+"\\")
--- stdnn/reporting/plotter.py
+++ stdnn/reporting/plotter.py
@@ -28,17 +28,17 @@ class Plotter:
         save_figure_format : str, optional
             the file format (PNG, JPEG, etc) for the plot that will be saved to an external directory, by default 'png'
         cmap_diverging_palette_husl_colours : list, optional
-            a float array containing the HUSL colours [h_neg, h_pos] for the extents of the heatmap of the correlation matrix, by default [150, 275]
+            a float array containing the HUSL colours [h_neg, h_pos] for the extents of the heatmap of the correlation matrix, by default [240, 10]
         """
         sns.set_theme(style=seaborn_theme, )
         corr_matrix = dataframe.corr()
-        mask = np.triu(np.ones_like(corr_matrix, dtype=bool))
         cmap = sns.diverging_palette(
             cmap_diverging_palette_husl_colours[0], cmap_diverging_palette_husl_colours[1], as_cmap=True)
         fig, ax = plt.subplots()
-        sns.heatmap(corr_matrix, mask=mask, cmap=cmap, vmax=.3, center=0,
-                    square=True, linewidths=.5, cbar_kws={"shrink": .5}).set(title="Correlation Matrix")
-        plt.savefig(filepath + "\\"+figure_name + "." + save_figure_format)
+        sns.heatmap(corr_matrix, cmap=cmap, center=0).set(
+            title="Correlation Matrix")
+        plt.tight_layout()
+        plt.savefig(filepath+"\\"+figure_name + "." + save_figure_format)
         plt.clf()
 
     @staticmethod
@@ -58,29 +58,5 @@ class Plotter:
         sns.lineplot(x='epochs', y='Accuracy', hue='Key',
                      data=pd.melt(dataframe, 'epochs', value_name="Accuracy", var_name="Key"), legend="auto")
 
-        plt.savefig(filepath + "\\"+figure_name + "." + save_figure_format)
+        plt.savefig(filepath + figure_name + "." + save_figure_format)
         plt.clf()
-
-# Filepath stuff
-
-
-absolutepath = os.path.abspath(__file__)
-fileDirectory = os.path.dirname(absolutepath)
-# Navigate to Images directory
-newPath = os.path.join(fileDirectory, 'images')
-
-
-# Generate a large random dataset - just here for testing purposes
-rs = np.random.RandomState(33)
-d = pd.DataFrame(data=rs.normal(size=(50, 26)),
-                 columns=list(ascii_letters[26:]))
-
-rs = np.random.RandomState(20)
-colnames = ['training_loss', 'validation_loss']
-df = pd.DataFrame(data=rs.normal(size=(50, 2)),
-                  columns=colnames)
-rng = np.arange(start=1, stop=51)
-df['epochs'] = rng
-
-Plotter.plot_correlation_matrix(dataframe=d, filepath=newPath)
-Plotter.plot_training_vs_validaion_loss(dataframe=df, filepath=newPath)
