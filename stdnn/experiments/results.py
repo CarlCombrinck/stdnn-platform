0a1c0d8598323399792d1208577d8bf63ba7d25b
Carl Combrinck <carljcombrinck@gmail.com>
2022-01-17 05:39:30 +0200

    Refactor Results class and add classes for Run and Experiment results

--- stdnn/experiments/results.py
+++ stdnn/experiments/results.py
@@ -3,20 +3,55 @@ import pandas as pd
 import os
 import sys
 
+# TODO Deep copies
 
-class Results:
-    global results_dict
-    results_dict = {}
+class Result():
+    """
+    Class for storing and managing the results of an experiment
+    """
 
-    def __init__(self):
-        return True
+    def __init__(self, results={}):
+        self.results = {}
+        for key, frame in results.items():
+            self.add_dataframe(frame, key)
 
-    def add_dataframe_to_dictionary(entry_name, dataframe):
-        results_dict[entry_name] = dataframe
+    def add_dataframe(self, dataframe, key):
+        self.results[key] = dataframe.copy(deep=True)
 
-    def retrieve_dataframe_from_dictionary(entry_name):
-        dataframe = results_dict.get(entry_name)
-        if dataframe is None:
-            return "Cannot retrieve object"
-        else:
-            return dataframe
+    def get_dataframe(self, key):
+        return self.results.get(key)
+
+    def get_dataframes(self):
+        return self.results
+
+    def get_dataframe_names(self):
+        return self.results.keys()
+
+    def copy(self):
+        return Result(self.results)
+
+    def __str__ (self):
+        return str(self.results)
+
+    def __repr__(self):
+        return "Result(\n" + str(self) + "\n)"
+
+class RunResult(Result):
+    """
+    Class representing the results of an experiment run
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def copy(self):
+        return RunResult(self.results)
+
+class ExperimentResult(Result):
+    """
+    Class representing the results of an experiment (combined runs)
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def copy(self):
+        return ExperimentResult(self.results)
